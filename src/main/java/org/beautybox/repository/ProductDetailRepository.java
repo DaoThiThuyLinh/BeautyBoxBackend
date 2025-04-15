7a93825744781670f0d56aa7043b35bda80a2f63
DaoThiThuyLinh <thuylinh2k3kd@gmail.com>
2025-04-15 23:41:18 +0700

    Fea/find-product-detail-by-id

--- src/main/java/org/beautybox/repository/ProductDetailRepository.java
+++ src/main/java/org/beautybox/repository/ProductDetailRepository.java
@@ -3,13 +3,19 @@ package org.beautybox.repository;
 import org.beautybox.entity.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, String> {
     @Query("from ProductDetail pd " +
             "where pd.product.id = :productId ")
     List<ProductDetail> findByProductId(String productId);
+    @Query("FROM ProductDetail pd " +
+            "WHERE pd.id = :productDetailId " +
+            "AND pd.isEnabled = true ")
+    Optional<ProductDetail> findById(@NonNull String productDetailId);
 }
